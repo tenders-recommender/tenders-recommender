114fb90d6c9246987a02a729f3ee6c4e8b554933
pmatejko <matejko.pawel@gmail.com>
2018-05-08 01:37:34 +0200

    added rmse plot using charts.js

--- frontend/src/app/rmse/rmse.component.ts
+++ frontend/src/app/rmse/rmse.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+
+import * as Chart from 'chart.js';
 
 import { RMSE } from '../rmse';
 import { ApiData } from '../api-data';
@@ -11,8 +13,10 @@ import { ApiData } from '../api-data';
 })
 export class RmseComponent implements OnInit {
   rmseHistory: RMSE[] = [];
+  points = [];
+  plot: Chart;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private elementRef: ElementRef) { }
 
   ngOnInit() {
     const url = 'http://127.0.0.1:5000/' + 'rmse/summary';
@@ -21,6 +25,38 @@ export class RmseComponent implements OnInit {
       data => {
         console.log(data);
         this.rmseHistory = ((data as ApiData).data as Array<any>).map(jsonObject => new RMSE(jsonObject));
+
+        for (const rmse of this.rmseHistory) {
+          const x = new Date(rmse.timestamp * 1000);
+          const y = rmse.rmse;
+          this.points.push({x: x, y: y});
+        }
+
+        const ctx = this.elementRef.nativeElement.querySelector('#rmsePlot').getContext('2d');
+        this.plot = new Chart(ctx, {
+          type: 'line',
+          data: {
+            datasets: [{
+              label: 'RMSE',
+              data: this.points,
+              backgroundColor: 'rgba(255,0,0,1)',
+              borderColor: 'rgba(255,0,0,1)',
+              fill: false
+            }]
+          },
+          options: {
+            scales: {
+              xAxes: [{
+                type: 'time',
+                time: {
+                  displayFormats: {
+                    minute: 'YYYY-MM-DD HH:mm'
+                  }
+                }
+              }]
+            }
+          }
+        });
       },
       err => {
         console.log(err);
