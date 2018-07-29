753cf98e76b02f2f75c2d395f035ad0a95ed0aa3
dawid <dawid.prokopek@gmail.com>
2018-07-29 19:54:03 +0200

    Added time_step test for KNNBasic

---
+++ recommender_surprise/time_step_test.py
@@ -0,0 +1,38 @@
+import random
+
+import numpy as np
+from surprise import KNNBasic, SVD
+from datetime import datetime, timedelta
+import pytz
+from recommender_surprise.recommender import Recommender
+
+
+def main():
+    seed = 0
+    random.seed(seed)
+    np.random.seed(seed)
+
+    k = 10
+    min_k = 3
+    sim_options = {'name': 'cosine',
+                   'min_support': 1,
+                   'user_based': False
+                   }
+    knn = KNNBasic(k=k, min_k=min_k, sim_options=sim_options)
+
+    utc = pytz.UTC
+    start_date = datetime.strptime('30 Oct 2015', '%d %b %Y').replace(tzinfo=utc)
+    end_date = datetime.strptime('20 Nov 2015', '%d %b %Y').replace(tzinfo=utc)
+
+    delta = timedelta(days=7)
+    while start_date <= end_date:
+        print(start_date.strftime("%Y-%m-%d"))
+        Recommender(earlier_than=start_date,
+                    algorithm=knn,
+                    alg_name=KNNBasic.__name__,
+                    rmse_file_name='Knn_rmse_summary.json')
+        start_date += delta
+
+
+if __name__ == '__main__':
+    main()
