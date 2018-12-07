b404373569bb68a7d483e153ac1c964d5ba4c24a
dawid <dawid.prokopek@gmail.com>
2018-12-07 01:05:13 +0100

    Add new table for test results

--- tenders_recommender/model/__init__.py
+++ tenders_recommender/model/__init__.py
@@ -2,3 +2,5 @@ from .parsed_data import ParsedData
 from .recommendation import Recommendation
 from .types import Interaction, Testset
 from .users_interactions import UsersInteractions
+from .test_results import TestResults
+from .result_types import ResultTypes
